b5ec6ea2b2d55116b421b4d1efd675526af2de53
王言 <frank.wy@alibaba-inc.com>
2017-04-28 00:32:12 +0800

    接入高德地图

--- TBox/AppDelegate.m
+++ TBox/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+     [AMapServices sharedServices].apiKey = @"86efef8475e1836b519997c0fae0b862";
+    
     return YES;
 }
 
